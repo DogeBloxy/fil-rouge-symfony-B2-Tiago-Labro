e1d91195c55300ce38c34ef05160af79c6b9c5fc
DogeBloxy <supertiago.260@gmail.com>
2025-02-03 13:26:11 +0100

    print announcements

--- project/src/Controller/AnnounceController.php
+++ project/src/Controller/AnnounceController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Announces;
+use App\Repository\AnnouncesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,16 +14,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AnnounceController extends AbstractController
 {
     #[Route('/announce', name: 'announce.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('announce/index.html.twig');
+        $repository = $entityManager->getRepository(Announces::class);
+
+        $announces = $repository->findAll();
+        return $this->render('announce/index.html.twig', [
+            'announces' => $announces
+        ]);
     }
-    #[Route('/announce/{slug}-{id}', name: 'announce.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    #[Route('/announce/{id}', name: 'announce.show', requirements: ['id' => '\d+'])]
+    public function show(Request $request, int $id, EntityManagerInterface $entityManager): Response
     {
+
+        $repository = $entityManager->getRepository(Announces::class);
+
+        $announce = $repository->find($id);
+
         return $this->render('announce/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id
+            'announce' => $announce
         ]);
     }
 }
